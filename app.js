e395793c8445fbcb0294dbdc850a917895484d08
RICHCHE <105933211+RICHCHE@users.noreply.github.com>
2024-10-12 23:35:55 +0700

    21

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ var logger = require('morgan');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-// Mongoose connection
+// MongoDB connection
 mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://admin:1234@hightech.dmuzq.mongodb.net/?retryWrites=true&w=majority&appName=Hightech', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 })
 .then(() => console.log('MongoDB Connected'))
-.catch(err => console.error('MongoDB Connection Error:', err));
+.catch(err => console.error('MongoDB Connection Failed:', err));
 
 var indexRouter = require('./routes/index');
 var documentsRouter = require('./routes/documents');
@@ -23,21 +23,19 @@ var app = express();
 // Enable CORS
 app.use(cors());
 
-// Static files
-app.use(express.static(path.join(__dirname, 'public')));
-
 // Middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.use('/api', indexRouter);
 app.use('/documents', documentsRouter);
 app.use('/users', usersRouter);
 
-// Catch 404 and forward to error handler
+// Catch 404
 app.use(function(req, res, next) {
   next(createError(404));
 });
